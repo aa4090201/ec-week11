5dc66ed433afd3ad684ad65ade8075528960b771
aa4090201 <91879943+aa4090201@users.noreply.github.com>
2021-11-30 11:42:37 +0800

    Add files via upload

---
+++ BLE_SendText.ino
@@ -0,0 +1,23 @@
+#include <BluetoothSerial.h>
+BluetoothSerial BT;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  BT.begin("GUNLINLIANG");//請改名
+  pinMode(14,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  while (BT.available()) {
+    //讀取藍芽資料
+    String BTdata=BT.readString();
+    if (BTdata=="LED OFF"){
+      digitalWrite(14,LOW);
+      }
+      else digitalWrite(14,HIGH);
+    //顯示在序列視窗
+    Serial.println(BTdata);
+  }
+  delay(1);
+}
